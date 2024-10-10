af09df98293a6edfd60fa86f18ddbe07a510d6a2
ethankook <93116378+ethankook@users.noreply.github.com>
2024-10-10 11:08:38 -0700

    finished favorites list

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,18 +2,48 @@ import { Label, Note } from "./types"; // Import the Label type from the appropr
 import { dummyNotesList } from "./constant"; // Import the dummyNotesList from the appropriate module
 import './App.css';
 import { ClickCounter } from "./hooksExercise";
+import React, { useState, useEffect, useContext } from 'react';
+import {ThemeContext, themes} from "./themeContext";
 
 function App() {
+  const [favorites, setFavorites] = useState<string[]>([]);
+
+  const toggleFavorite = (title: string) => {
+    if (favorites.includes(title)) {
+      setFavorites(favorites.filter(favorite => favorite !== title));
+    } else {
+      setFavorites([...favorites, title]);
+    }
+  };
+
+  useEffect(() => {
+    console.log(favorites);
+  }, [favorites]);
+
  return (
 
-   <div className='app-container'>
+   <div className={`app-container`}>
          <form className="note-form">
        <div><input placeholder="Note Title"></input></div>
 
-       <div><textarea></textarea></div>
+       <div><textarea placeholder="Note Content"></textarea></div>
+       <div id = "labelForm">      
+         <select name="label" id="label">
+                <option value="Personal">Personal</option>
+                <option value="Work">Work</option>
+                <option value="Study">Study</option>
+                <option value="Other">Other</option>
+      </select>
+      </div>
 
        <div><button type="submit">Create Note</button></div>
       </form>
+      <div className="favorites-list">
+        <h2>Favorites</h2>
+        {favorites.map((favorite, index) => (
+          <p key={index}>{favorite}</p>
+        ))}
+      </div>
       <div className="notes-grid">
        {dummyNotesList.map((note) => (
          <div
@@ -21,6 +51,9 @@ function App() {
            className="note-item">
            <div className="notes-header">
              <button>x</button>
+             <button onClick={() => toggleFavorite(note.title)}>
+                {favorites.includes(note.title) ? '❤️' : '♡'}
+              </button>
            </div>
            <h2> {note.title} </h2>
            <p> {note.content} </p>
@@ -36,5 +69,4 @@ function App() {
 
  );
 }
-
 export default App;
